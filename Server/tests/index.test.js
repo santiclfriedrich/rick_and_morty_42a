9629f84c617dadbcb4f16f0e79420f5f17dd744f
santiclfriedrich <santiclear1@hotmail.com>
2023-09-13 02:08:49 -0300

    M3-Test || Test de pruebas

---
+++ Server/tests/index.test.js
@@ -0,0 +1,106 @@
+const app = require('../src/app');
+const session = require('supertest');
+const agent = session(app);
+
+describe("Test de Rutas", ()=>{
+    describe("GET /rickandmorty/character/:id", () => {
+        it("Responde con status: 200", async () => {
+            await agent.get('/rickandmorty/character/1').expect(200);
+        });
+        it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image" ', async () => {
+            const {body} = (await agent.get('/rickandmorty/character/1'))
+
+            const atributes = [ 
+            "id",
+            "name",
+            "species",
+            "gender",
+            "status",
+            "origin",
+            "image",
+            ];
+
+            const keys = Object.keys(body)
+
+            atributes.forEach((atribute) => {
+                expect(keys).toContain(atribute)
+            });
+
+
+        });
+
+        it('Si hay un error responde con status: 500', async () => {
+            await agent.get('/rickandmorty/character/bartolomiau').expect(500)
+        });
+    });
+
+    describe('GET /rickandmorty/login', () => {
+        it('Informacion sea correcta y nos de acceso', async() => {
+
+            const {body} = await agent.get( "/rickandmorty/login?email=santi@gmail.com&password=santi123");
+
+            expect(body.access).toEqual(true)
+
+        })
+
+        it('Informacion sea incorrecta y nos bloquea', async() => {
+
+            const {body} = await agent.get( "/rickandmorty/login?email=santi@gmail.com&password=cualquiera");
+
+            expect(body.access).toEqual(false)
+
+        })
+
+    })
+
+    describe("POST /rickandmorty/fav", () => {
+
+        const char1 = {id:1, name:"Santi"}
+        const char2 = {id:2, name:"Jorge"}
+
+        it("Devuelve un array con el personaje", async () => {
+
+            const {body} = (await agent.post("/rickandmorty/fav").send
+            (char1));
+
+            expect(body).toContainEqual(char1)
+
+        })
+
+        it("Al enviar mas de un elemento devuelve todos los elementos", async ()=>{
+
+            const {body} = await agent.post("/rickandmorty/fav").send
+            (char2);
+
+            expect(body).toContainEqual(char1)
+            expect(body).toContainEqual(char2)
+
+        })
+
+    })
+
+    describe("DELETE /rickandmorty/fav/:id", () => {
+
+        const char1 = {id:1, name:"Santi"}
+        const char2 = {id:2, name:"Jorge"}
+
+        it("Si no se envia un ID correcto se devuelve el mismo array", async () => {
+
+
+            const {body} = await agent.delete("/rickandmorty/fav/21102")
+
+            expect(body).toContainEqual(char1)
+            expect(body).toContainEqual(char2)
+        })
+
+        it("Elimina correctamente al personaje que contenga el ID", async () => {
+
+            const {body} = await agent.delete("/rickandmorty/fav/1")
+
+            expect(body).not.toContainEqual(char1)
+
+        })
+
+    })
+
+})
