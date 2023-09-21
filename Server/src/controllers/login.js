d587fc25b8fa7f569eba3ab2f904fd50c04f221e
santiclfriedrich <santiclear1@hotmail.com>
2023-09-21 02:15:15 -0300

    M4-ORM2 || Cambios en los controladores || Solucionar error axios al agregar a favs

---
+++ Server/src/controllers/login.js
@@ -0,0 +1,24 @@
+const {User} = require('../DB_connection');
+
+module.exports = async (req, res) => {
+
+    try {
+
+        const {email, password} = req.query;
+
+        if(!email || !password) return res.status(400).send("Faltan datos");
+
+        const user = await User.findOne({where:{email}});
+
+        if(!user) return res.status(404).send("Usuario no encontrado");
+
+        return user.password === password
+        ? res.json({access:true})
+        : res.status(403).send("Contraseña incorrecta")
+    } catch (error) {
+
+        return res.status(500).send(error.message);
+        
+    }
+
+}
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,13 +1,19 @@
 const {Router} = require('express');
+const router = Router();
+
 const getCharById = require('../controllers/getChardById');
-const { postFav, deleteFav } = require('../controllers/handleFavorites');
-const login = require('../controllers/loginController');
+const login = require('../controllers/login');
+const postUser = require('../controllers/postUser');
+const postFav = require('../controllers/postFav');
+const deleteFav = require('../controllers/deleteFav');
 
-const router = Router()
 
-router.get('/character/:id', getCharById)
+
 router.get('/login', login);
+router.post('/login', postUser);
 router.post('/fav', postFav);
 router.delete('/fav/:id', deleteFav)
+router.get('/character/:id', getCharById)
+
 
 module.exports = router;
